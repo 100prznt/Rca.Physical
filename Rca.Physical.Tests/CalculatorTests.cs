1aafb2f6abd3ba486ea88dd125bfd7186b5802fb
Elias Ruemmler <46575176+100prznt@users.noreply.github.com>
2023-02-23 19:41:26 +0100

    Update CalculatorTests.cs

--- Rca.Physical.Tests/CalculatorTests.cs
+++ Rca.Physical.Tests/CalculatorTests.cs
@@ -11,5 +11,24 @@ namespace Rca.Physical.Tests
         {
             Calculator.TryMultiply(new PhysicalValue(12, PhysicalUnits.Volt), new PhysicalValue(1, PhysicalUnits.Celsius), out var result);
         }
+
+        [TestMethod]
+        public void MultiplicationVelocityAndArea()
+        {
+            Calculator.TryMultiply(new PhysicalValue(1.731, PhysicalUnits.MetrePerSecond), new PhysicalValue(0.0016045998637475229, PhysicalUnits.SquareMetre), out var result);
+
+            Assert.AreEqual(10, result.ValueAs(PhysicalUnits.CubicMetrePerHour), 1E-3);
+        }
+
+        
+
+         [TestMethod]
+        public void DivisionFlowRateByArea()
+        {
+            Calculator.TryDivide(new PhysicalValue(5, PhysicalUnits.CubicMetrePerHour), new PhysicalValue(0.0016045998637475229, PhysicalUnits.SquareMetre), out var result);
+
+            Assert.AreEqual(0.866, result.ValueAs(PhysicalUnits.MetrePerSecond), 1E-3);
+        }
+
     }
 }
